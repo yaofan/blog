5e4acfb8cdd41ec164ec32f7eff462cde62f2e69
梵森 <yaofan.yf@alibaba-inc.com>
2014-06-13 11:26:00 +0800

    change proxy angian

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '3000'
+            port: '2368'
         },
         paths: {
             contentPath: path.join(__dirname, '/content/')
@@ -61,7 +61,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '3000'
+            port: '2368'
         }
     },
 
